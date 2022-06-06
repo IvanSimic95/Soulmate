6c36cda58e5aa548ad78ad52be481dfff935adc5
s0mekind <gabeobm@gmail.com>
2022-06-06 19:34:27 +0300

    a

--- index.php
+++ index.php
@@ -1,93 +1,74 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:title" content="Ready To Meet Your Soulmate?">
-    <meta property="og:description" content="Draw Your Soulmate Today !">
-    <meta property="og:url" content="https://soul-mate.shop">
-    <meta property="og:type" content="website">
-    <title>Ready To Meet Your Soulmate?</title>
+<?php $title = "Melissa Psychic | Homepage"; ?>
+<?php $description = "I can read anything for you with my psychic abilities"; ?>
+<?php $menu_order = "men_1_0"; ?>
 
-    
+<?php 
+include_once $_SERVER['DOCUMENT_ROOT'].'/config/vars.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/assets/templates/header.php'; ?>
 
-    <!-- Bootstrap core CSS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<section class="hero_slider">
+  <div class="dark_bg"></div>
+    <div class="container">
+      <div class="hero_box">
+        <div class="hero_text">
+          <h1><span>I am Melissa, </span>
+          <h2> a forceful and energetic Modern Psychic Expert, a gifted artist with the ability of clairvoyance, clairaudience, claircognizance, and clairsentience. The purpose of my intuitive gifts is to give you a greater understanding of love and life, which in turn, changes the quality and experience of your life, career and health.. </h2>
+          
+          <a class="btn" href="https://www.soul-mate.shop/about.php">READ MORE</a>
+        </div>
+      </div>
+    </div>
+</section>
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/assets/templates/product_grid.php'; ?>
+<section class="about" style="background: #f3f4f7; padding: 40px 0;">
+  <div class="container">
+  <div class="white-wrapper about-wrap"> <h1>About Me</h1>
+    <div class="paragraph_area">
+      <div class="sides">
+        <div class="left">
+            <p><img src="/assets/img/about.jpg" alt="Melissa"></p>
+        </div>
+        <div class="right">
+          <div class="paragraph">
+            <p>Welcome to your safe place! My name is Melissa and I am a forceful and energetic Modern Psychic Expert, a gifted artist with the ability of clairvoyance, clairaudience, claircognizance, and clairsentience. The purpose of my intuitive gifts is to give you a greater understanding of love and life, which in turn, changes the quality and experience of your life, career, and health. </p>
+            <p>I have always had a special understanding of the past, an understanding acquired through my paranormal abilities and through contact with the person whose past is to be deciphered. In addition, I am also a specialized medium. </p>
+			<p>You can consider Clairvoyance a remote view - thus, it is not necessary to have physical contact with you, because I can decipher your problems by connecting with your energy and aura. By simply communicating with a person with problems, I have access to the energies that have influenced their past and that can influence their future.</p>
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-      body{
-        background: url(index/bg.jpg);
-        background-size: cover !important;
-    -webkit-background-size: cover !important;
-    background-attachment: fixed !important;
-    background-repeat: repeat repeat !important;
-    background-position: center center;
-      }
-      a:link {
-  text-decoration: none;
-}
-
-a:visited {
-  text-decoration: none;
-}
-    </style>
-<script>
-		!function (w, d, t) {
-		  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++
-)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=i+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
-		
-		  ttq.load('CA17H73C77UADFMAP320');
-		  ttq.page();
-		}(window, document, 'ttq');
-	</script>
-    
-  </head>
-  <body>
-    
-<div class="col-lg-8 mx-auto p-3 py-md-5">
  
-
-  <main>
-    <div class="container" style="max-width:1050px;">
-    <h1 style="font-size:2rem;font-weight:bold;color:white;text-align:center;">Are You Wondering Who Your Soulmate Is?</h1>
-    <div class="row g-2">
-      <div class="col-6">
-      <img src="index/male.jpg" class="img-thumbnail" alt="Male">
+		  </div>
+        </div>
       </div>
-
-      <div class="col-6">
-      <img src="index/female.jpg" class="img-thumbnail" alt="Female">
-      </div>
-      <a href="https://soul-mate.shop/soulmate-drawing.php" type="button" class="btn btn-danger btn-block" style="font-size:3rem;font-weight:bold;">Yes, I Want My Soulmate!</a>
     </div>
+    <div class="about_more">
 
-    </div>
-  </main>
-  <footer class="pt-5 my-5 text-muted border-top">
-  <ul style="text-align:center;">    
-<li class="nav1 nodoNavItem" style="display: inline-block;padding-right: 30px;"><a href="https://soul-mate.shop/ab.html" class="nav1link nodoNavLink" target="_blank" style="color: rgb(255, 255, 255);">About Us</a></li>
-<li class="nav2 nodoNavItem" style="display: inline-block;padding-right: 30px;"><a href="https://www.soul-mate.shop/privacy-policy.html" class="nav2link nodoNavLink" target="_blank" style="color: rgb(255, 255, 255);"> Privacy Policy</a></li>
-<li class="nav3 nodoNavItem" style="display: inline-block;padding-right: 30px;"><a href="https://www.soul-mate.shop/terms.html" class="nav3link nodoNavLink" target="_blank" style="color: rgb(255, 255, 255);"> Terms &amp; Conditions</a></li>
-</ul>
-  </footer>
-</div>
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-      
-  </body>
-</html>
+<p>
+When I decide to help people by using spirits, I enter a trance with the help of deep meditation. Trance is the ability to communicate with tangible or intangible entities to receive information from them. Depending on the problem I have to solve, I can assess whether I need to go into a deep trance or not, and connect with your Higher Self so that I can see exactly what may cause you pain, sadness or regrets.
+</p><p>
+Think of all the energy you spent throughout life on hating yourself because of how you look
+or how you feel. Instead, think of how much better you could be if you took that energy and invested it in your body, taking care of it and loving it. This is where I intervene. The trance that I just told you about will help me connect with your higher soul and identify the problems you are struggling with, even if they are related to money, career, love, health, or social issues.
+</p><p>
+I am a strong empath and have high sensitivity to the emotional vibrations of my clients. I have extraordinary intuition and foresight when it comes to accessing and understanding the emotions and feelings of my clients on a deeper and more meaningful level. I also have the power to tap into your life energy fields. I can give a reading on the general aspects of your life, or your specific life energy vibration like for love life, career, health, or relationships.
+</p><p>
+I have worked in important fields, and throughout my career I collaborated with the government, the law enforcement and many other institutions that needed my help to identify certain individuals. My services are appreciated by thousands of people from all over the world and I am grateful for every “thank you” message received after a reading, a soulmate drawing, or a divine soul purification. Being in this field for so many years has taught me that the Universe works for believers. If you believe that I can help you, ease your life and solve your problems, then the Universe will work for me, for us. Trust is an essential thing when it comes to psychic reading.
+</p><p>
+Skilled and forceful psychics like me are not limited by interactions in the same room with their clients. Even though great distances, I am still able to tap into the life energy fields and give accurate readings and solutions to your problems.
+</p></div>
+  </div>
+  </div>
+</section>
+<section class="reviews">
+  <div class="container">
+    <h2>But dont take my word, here what my past clients say:</h2>
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/assets/templates/reviews-home.php'; ?>
+  </div>
+</section>
+<!--<section class="subscribe">
+  <div class="container">
+    <h2>Subscribe to my newsletter to be notice when i post on my blog or add new products and offers</h2>
+    <form class="sub_box" action="#" method="post">
+      <input class="sub_email" type="email" name="sub_email" id="sub_email" value="" placeholder="Enter your email address">
+      <input class="sub_submit" type="submit" name="sub_submit" value="Sign me!">
+    </form>
+  </div>
+</section>-->
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/assets/templates/footer.php'; ?>
