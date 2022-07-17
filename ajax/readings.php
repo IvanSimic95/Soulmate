1e888cb3429b8d774b98f37c9ec0ad2b6d2f8081
IvanSimic95 <email@isimic.com>
2022-07-17 11:03:08 +0200

    Update readings.php

--- ajax/readings.php
+++ ajax/readings.php
@@ -86,7 +86,7 @@ switch ($ReadingsCounter){
   }
 
 $sql = "INSERT INTO orders (cookie_id, user_age, first_name, last_name, user_name, birthday, order_status, order_date, order_email, order_product, order_product_nice, order_priority, order_price, buygoods_order_id, user_sex, genderAcc, pick_sex, fbc, fbp) VALUES ('$cookie_id', '$user_age', '$fName', '$lName', '$user_name', '$user_birthday', '$oStatus', '$order_date', '$bgemail', '$order_product', '$order_product_nice', '$order_priority', '$pricenow', '', '$userGender', '$userGenderAcc', '$partnerGender', '$uFBC', '$uFBP')";
-
+$lastRowInsert = mysqli_insert_id($conn);
 if(mysqli_query($conn,$sql)){
 $submitStatus = "Success";
 $SuccessMessage = "Information saved, Redirecting you to Payment Page Now!";
