1c7ca5e30b1d8be323ac625c3ef2f81455899f39
IvanSimic95 <email@isimic.com>
2022-08-24 14:56:45 +0200

    Update footer.php

--- assets/templates/footer.php
+++ assets/templates/footer.php
@@ -70,14 +70,7 @@ src="https://www.facebook.com/tr?id=<?php echo $FBPixel; ?>&ev=PageView&noscript
       <div class="paragraph">
       Disclaimer: The information contained herein should not be used as a substitute for the advice of appropriately qualified and licensed person. According to the laws in force, I must state that my services are for entertainments purposes only. I have no liability and/or responsibility for any actions and/or decisions any buyer/client chooses to take or make based on his/her consultation. You  acknowledge that I am not a licensed psychologist, lawyer, or health care professional and my services do not replace the care of lawyers, psychologists, or other healthcare professionals. Tarot and numerology are in no way to be construed or substituted as psychological counseling or any other type of therapy or medical advice. I will at all times exercise my best professional efforts, skills, and care.</div>
       <hr>
-      <span id=disclaimer-bg></span>
-<script src="https://display.buygoods.com/v1/disclaimer?id=disclaimer-bg&account_id=6274"></script>
-<hr>
-<style>
-  #disclaimer-bg > div > div{
-    background:transparent!important;
-  }
-  </style>
+  
       <div class="sides">
           <div class="quarter">
             <h3>From Melissa</h3>
