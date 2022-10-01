5cb49891bb6257c26920646ada2fa6aae60d0c3b
s0mekind <gabeobm@gmail.com>
2022-10-01 21:41:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -932,8 +932,15 @@ var product_code = $('.product_code').text()
       <p class="sub-paragraph">I am <b>Isabella</b>, the most renowned psychic artist in the world, and I guarantee that I will find your true soulmate in this lifetime and reunite both of you, as well as make sure that you two will live a life full of joy and fulfillment on all plans.</p>
       <div class="w-row">
         <div class="w-col w-col-7 w-col-small-small-stack w-col-tiny-tiny-stack"><center> <img src="/images/polaroid2.png" loading="lazy" srcset="/images/polaroid2.png 500w, /images/polaroid2.png 571w" sizes="100vw" alt="" class="image-mobile"> </center>
-          <div class="about mobile">&quot;My heritage extends from a centuries-old family line of spiritual healers. I learned most of my castings from my great aunt, who like myself was a spiritual healer, psychic medium and empath!&quot;<br><strong>- Priestess Alice</strong></div>
-          <ul role="list" class="list">
+          <div class="about mobile">&quot;My heritage extends from a centuries-old family line of spiritual healers. I learned how to master my power from my great aunt, who like myself was a spiritual healer, psychic medium and empath!&quot;<br><strong>- Isabella </strong></div>
+	   
+		  <p><b>Do you want to know who your Soulmate is? What is the character of this person? Where and when you will meet them?   </b> </p> 
+		   <p>   <center> <b> You can find all the answers today! </b> </center> </p> 
+		    <p>  You see dear, you deserve to be completely happy and in love with your Soulmate but for some reason you may not be right now. Or you have a partner but you don't know if he is the chosen one. </p> 
+			<p> I’m a Psychic with more than 18 years of experience and my Psychic abilities are recognized worldwide, having successful collaborations including with the police in making robot portraits.  </p> 
+			<p>  I will combine my psychic abilities with my natural artisitc talent to transfer my vision to paper for you. You will receive a drawing of the person with whom you are going to be with for the long term, perhaps going to marry. You will also receive a detailed description of his/her characteristics and personality. </p> 
+			
+		<ul role="list" class="list">
             <li class="list-item">
               <div class="bullet-headline">DIGITAL PRODUCT</div>
               <div class="bullet-description"> All orders are delivered digitally to your email address</div>
